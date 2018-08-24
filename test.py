379f39a1f94bf49f7bc4d2c4cf2c7cd7928ba2b5
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-24 17:33:22 +0300

    created a test called test_save_contact that calls a save_contact method to our newly generated object.

--- test.py
+++ test.py
@@ -3,26 +3,35 @@ from user import User
 
 class TestUser(unittest.TestCase):
 
-  '''
-  Test class that defines test cases for the user class
+      '''
+      Test class that defines test cases for the user class
 
-  Args:
-      unittest.TestCase: TestCase class helping to create test cases
-  '''
+      Args:
+          unittest.TestCase: TestCase class helping to create test cases
+      '''
 
-def setUp(self):
-  '''
-  Set up method to run before each test cases.
-  '''
-  self.username = User("Gift","Gift-Lumumba","gL0711419032") #creates user object
+      def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Gift-Lumumba","gL0711419032") #creates user object
 
-def test_init(self):
-  '''
-  test_init test case to test if the object is initialized properly
-  '''
+      def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
 
-  self.assertEqual(self.new_user.username,"Gift-Lumumba")
-  self.assertEqual(self.new_user.password,"gL0711419032")
+        self.assertEqual(self.new_user.username,"Gift-Lumumba")
+        self.assertEqual(self.new_user.password,"gL0711419032")
+
+      def test_save_contact(self):
+        '''
+        test_save_contact test case to test if the user object has been saved into the user list
+        '''
+        self.new_user.save_user() #saving new user
+
+        self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()
