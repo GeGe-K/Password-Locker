89a053a33530d6bb5318f4061da67fe312e2e3b6
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-24 22:09:36 +0300

    added tearDown method and assigned user_list list in User class an empty list

--- test.py
+++ test.py
@@ -31,6 +31,21 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()#saving new user
         self.assertEqual(len(User.user_list),1)
 
+      def tearDown(self):
+        '''
+        tearDown method which does clean up after each test has run.
+        '''
+        User.user_list = [] 
+
+      def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","gL1999")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
 
 if __name__ == '__main__':
     unittest.main()
