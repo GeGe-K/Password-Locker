0f6e08f41d3c9e44b787fc389eb407963dc24e40
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-24 17:09:16 +0300

    created self contact method cc

--- user.py
+++ user.py
@@ -4,16 +4,24 @@ class User:
 
   """
 
-  password_list = [] #Empty password list
+  user_list = [] #Empty password list
+  def save_user(self):
 
-def __init__(self,username,password):
-  '''
-  __init__ method for definition of object properties.
+    '''
+    save_user method saves user objects into user_list
+    '''
 
-  Args:
-      username:Username of the new user.
-      password:Password of the new user.
+    User.user_list.append(self)
 
-  '''
-  self.username = username 
-  self.password = password
+  def __init__(self,username,password):
+    '''
+    __init__ method for definition of object properties.
+
+    Args:
+        username:Username of the new user.
+        password:Password of the new user.
+
+    '''
+  
+    self.username = username 
+    self.password = password
